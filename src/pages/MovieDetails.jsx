faa2b762cbc302f4523ff1fe6f6a275eabe5e15d
ekv86 <ekv8686@gmail.com>
2023-08-01 14:15:15 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import { getFilmDetails } from 'filmApi';
-import { useState, useEffect, Suspense } from 'react';
+import { useState, useEffect, Suspense, useRef } from 'react';
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import css from '../components/MovieDetails.module.css';
 import imageError from 'no-photo.png';
@@ -7,8 +7,9 @@ import imageError from 'no-photo.png';
 const MovieDetails = () => {
   const { id } = useParams();
   const location = useLocation();
-  const additionalLocation = location.state;
-  const backBtn = location.state?.from ?? '/';
+  console.log(location)
+  // const additionalLocation = location.state;
+  const backBtn = useRef(location.state?.from ?? '/');
   const [film, setFilm] = useState({} ?? '');
   const { title, poster_path, overview, genres, vote_average } = film;
   const imgPath = 'http://image.tmdb.org/t/p/w500';
@@ -28,7 +29,7 @@ const MovieDetails = () => {
 
   return (
     <main>
-      <Link to={backBtn} className={css.back__btn}>
+      <Link to={backBtn.current} className={css.back__btn}>
         Go back
       </Link>
       {!film && <p>The resource you requested could not be found</p>}
@@ -49,12 +50,12 @@ const MovieDetails = () => {
             <p>Additional information</p>
             <ul>
               <li className={css.film__additional_item}>
-                <Link to="cast" state={{ from: additionalLocation.from }}>
+                <Link to="cast" state={{ from: location }}>
                   Cast
                 </Link>
               </li>
               <li className={css.film__additional_item}>
-                <Link to="reviews" state={{ from: additionalLocation.from }}>
+                <Link to="reviews" state={{ from: location }}>
                   Reviews
                 </Link>
               </li>
